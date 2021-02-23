6b3c318a66fa5e644c9c40d5ba66e7564071b4a0
Raito KAGAWA <71278326+raitendirekt@users.noreply.github.com>
2021-02-23 18:26:26 +0900

    stylesheetsの配置

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :nickname, presence: true
+  validates :family_name, presence: true
+  validates :first_name, presence: true
+  validates :kana_family_name, presence: true
+  validates :kana_first_name, presence: true
 end
