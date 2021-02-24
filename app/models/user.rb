34ce0e1f6d568b08ffa3f12b0f06377a20c0b16d
Raito KAGAWA <71278326+raitendirekt@users.noreply.github.com>
2021-02-24 11:39:08 +0900

    エラーメッセージ、バリテーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :nickname, presence: true
-  validates :family_name, presence: true
-  validates :first_name, presence: true
-  validates :kana_family_name, presence: true
-  validates :kana_first_name, presence: true
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'must be full-width characters' } do
+    validates :family_name
+    validates :first_name
+  end
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'must be full-width katakana characters' } do
+    validates :kana_family_name
+    validates :kana_first_name
+  end
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'must include both letters and numbers'
 end
