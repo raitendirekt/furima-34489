5fae41d75110015b4e2a3bc72a0177eb3463a742
Raito KAGAWA <71278326+raitendirekt@users.noreply.github.com>
2021-02-25 21:48:35 +0900

    単体テストコードの記述

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,62 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '#create' do
+    context 'when valid' do
+      it 'is valid with valid attributes' do
+        expect(@item).to be_valid
+      end
+    end
+    context 'when not valid' do
+      it 'is not valid without an image' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+      it 'is not valid without a name' do
+        @item.name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+      end
+      it 'is not valid without a description' do
+        @item.description = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Description can't be blank")
+      end
+      it 'is not valid without a category' do
+        @item.category_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category is not a number")
+      end
+      it 'is not valid without a condition' do
+        @item.condition_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Condition is not a number")
+      end
+      it 'is not valid without a bear shipping' do
+        @item.bear_shipping_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Bear shipping is not a number")
+      end
+      it 'is not valid without a prefecture' do
+        @item.prefecture_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture is not a number")
+      end
+      it 'is not valid without a shipment day' do
+        @item.shipment_day_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipment day is not a number")
+      end
+      it 'is not valid without a price' do
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+    end
+  end
 end
