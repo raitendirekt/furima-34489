68ef58def99ec2fe0e691c897b825907d1bb13b9
Raito KAGAWA <71278326+raitendirekt@users.noreply.github.com>
2021-02-25 15:44:00 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,6 +7,15 @@ class Item < ApplicationRecord
   belongs_to :bear_shipping
   belongs_to :prefecture
   belongs_to :shipment_day
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :category_id, numericality: { other_than: 1 }
+  validates :condition_id, numericality: { other_than: 1 }
+  validates :bear_shipping_id, numericality: { other_than: 1 }
+  validates :prefecture_id, numericality: { other_than: 1 }
+  validates :shipment_day_id, numericality: { other_than: 1 }
+  validates :price, presence: true
+  validates :image, presence: true
 
   private
 
