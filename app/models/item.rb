8bf8982e87b5a9657b62ab943f1180247978ce71
Raito KAGAWA <71278326+raitendirekt@users.noreply.github.com>
2021-02-26 12:06:19 +0900

    テストコードの追記

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,14 +7,17 @@ class Item < ApplicationRecord
   belongs_to :bear_shipping
   belongs_to :prefecture
   belongs_to :shipment_day
-  validates :name, presence: true
-  validates :description, presence: true
-  validates :category_id, numericality: { other_than: 1 }
-  validates :condition_id, numericality: { other_than: 1 }
-  validates :bear_shipping_id, numericality: { other_than: 1 }
-  validates :prefecture_id, numericality: { other_than: 1 }
-  validates :shipment_day_id, numericality: { other_than: 1 }
-  validates :price, presence: true
-  validates :image, presence: true
-
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :description
+    validates :price, inclusion: { in: 300..9_999_999 }, format: { with: /\A[0-9]+\z/ }
+  end
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :condition_id
+    validates :bear_shipping_id
+    validates :prefecture_id
+    validates :shipment_day_id
+  end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -57,6 +57,21 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Price can't be blank")
       end
+      it 'is not valid if price is less than 300' do
+        @item.price = '299'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not included in the list")
+      end
+      it 'is not valid if price is more than 9,999,999' do
+        @item.price = '10_000_000'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not included in the list")
+      end
+      it 'is not valid if price is not half-width number' do
+        @item.price = '３００'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not included in the list")
+      end
     end
   end
 end
