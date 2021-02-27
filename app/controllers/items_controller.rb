b1186539a0266b217635e27b148b0f7db9f033b1
Raito KAGAWA <71278326+raitendirekt@users.noreply.github.com>
2021-02-27 10:48:56 +0900

    多用する変数をprivate以下にまとめる

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,8 @@
 class ItemsController < ApplicationController
+  before_action :set_item, except: [:index :new, :create]
   before_action :authenticate_user!, only: [:new, :create, :edit, :destroy]
+  before_action :seller_confirmation, only: [:edit, :update, :destory]
+
   def index
     @items = Item.all.order('created_at DESC')
   end
@@ -18,16 +21,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
-    redirect_to action: :index unless current_user.id == @item.user_id
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path
     else
@@ -40,5 +39,13 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :bear_shipping_id, :prefecture_id,
                                  :shipment_day_id, :price).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def seller_confirmation
+    redirect_to action: :index unless current_user.id == @item.user_id
   end
 end
