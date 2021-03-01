41354c472bdde01d877e869483397f996a9e59c8
Raito KAGAWA <71278326+raitendirekt@users.noreply.github.com>
2021-03-01 15:38:16 +0900

    購入情報の保存

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,16 +1,25 @@
 class PurchasesController < ApplicationController
+  before_action :authenticate_user!
   def index
+    @purchase_address = PurchaseAddress.new
+    @item = Item.find(params[:item_id])
+  end
+
+  def create
+    @item = Item.find(params[:item_id])
     @purchase_address = PurchaseAddress.new(purchase_params)
+    binding.pry
     if @purchase_address.valid?
       @purchase_address.save
       redirect_to root_path
     else
-      render :new
+      render :index
     end
   end
 
   private
 
   def purchase_params
-    params.require(:purchase_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :mobile_number).merge(user_id: current_user.id, :item_id, :purchase_id)
+    params.require(:purchase_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :mobile_number).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_27_070739) do
+ActiveRecord::Schema.define(version: 2021_03_01_061007) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -39,7 +39,7 @@ ActiveRecord::Schema.define(version: 2021_02_27_070739) do
     t.string "city", null: false
     t.string "house_number", null: false
     t.string "building_name"
-    t.integer "mobile_number", null: false
+    t.bigint "mobile_number", null: false
     t.bigint "purchase_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
